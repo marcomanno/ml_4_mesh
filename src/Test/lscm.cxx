da75a17056a2afbcee341bb9cc5793f060f1962c
galiautdinova gyuzel <guzel.gala@outlook.com>
2018-10-08 23:56:47 +0100

    Add non linear flattening

--- src/Test/lscm.cxx
+++ src/Test/lscm.cxx
@@ -1,6 +1,8 @@
 //#pragma optimize("", off)
 #include "catch.hpp"
 
+#include "optimize_function.hxx"
+
 #include "Geo/vector.hh"
 #include "Import/load_obj.hh"
 #include "Import/save_obj.hh"
@@ -10,7 +12,7 @@
 #include <Eigen/SparseQR>
 
 
-auto move_to_local_coord(const Geo::VectorD3 _tri[3], Geo::VectorD2 _loc_tri[2])
+static auto move_to_local_coord(const Geo::VectorD3 _tri[3], Geo::VectorD2 _loc_tri[2])
 {
   auto v_01 = _tri[1] - _tri[0];
   _loc_tri[0][0] = Geo::length(v_01);
@@ -22,10 +24,38 @@ auto move_to_local_coord(const Geo::VectorD3 _tri[3], Geo::VectorD2 _loc_tri[2])
   return area_time_2;
 }
 
-void flatten(Topo::Wrap<Topo::Type::BODY> _in_body)
+using VertexIndMpap = std::map<Topo::Wrap<Topo::Type::VERTEX>, size_t>;
+
+struct OptimizeNonLinear
+{
+  OptimizeNonLinear(const VertexIndMpap& _vrt_inds, Topo::Wrap<Topo::Type::BODY> _body):
+    vrt_inds_(_vrt_inds), body_(_body) { }
+
+  void compute(Eigen::VectorXd& _X)
+  {
+    Topo::Iterator<Topo::Type::BODY, Topo::Type::FACE> bf(body_);
+    auto q_solver = IQuadraticSolver::make();
+    q_solver->init(_X.size(), bf.size(), _X.data());
+    q_solver->compute(*this);
+  }
+
+  bool operator()(double* _x, double* _f, double* _fj)
+  {
+    return true;
+  }
+
+  const VertexIndMpap& vrt_inds_;
+  Topo::Wrap<Topo::Type::BODY> body_;
+};
+
+// Function to minimize 
+
+
+
+void flatten(Topo::Wrap<Topo::Type::BODY> _body)
 {
   std::map<Topo::Wrap<Topo::Type::VERTEX>, size_t> vrt_inds;
-  Topo::Iterator<Topo::Type::BODY, Topo::Type::FACE> bf(_in_body);
+  Topo::Iterator<Topo::Type::BODY, Topo::Type::FACE> bf(_body);
   using Triplet = Eigen::Triplet<double, size_t>;
   using Matrix = Eigen::SparseMatrix<double>;
   std::vector<Triplet> coffs;
@@ -79,24 +109,35 @@ void flatten(Topo::Wrap<Topo::Type::BODY> _in_body)
   solver.compute(A);
   Eigen::VectorXd X = solver.solve(b);
   std::cout << X.rows() << " " << X.cols() << std::endl;
+  X.resize(cols);
+  for (size_t i = 0; i < 4; ++i)
+    X(split_idx + i, 0) = fixed(i, 0);
+
+  OptimizeNonLinear onl(vrt_inds, _body);
+  onl.compute(X);
+
   for (const auto& v_id : vrt_inds)
   {
     auto base_ind = 2 * v_id.second;
     Geo::VectorD3 pt;
-    if (base_ind < split_idx)
-      pt = { X(base_ind, 0), X(base_ind + 1, 0), 0 };
-    else
-    {
-      auto idx = base_ind - split_idx;
-      pt = { -fixed(idx, 0), -fixed(idx + 1, 0), 0 };
-    }
+    pt = { X(base_ind, 0), X(base_ind + 1, 0), 0 };
     const_cast<Topo::Wrap<Topo::Type::VERTEX>&>(v_id.first)->set_geom(pt);
   }
+
+
+
 }
 
 TEST_CASE("flat_00", "[Flattening]")
 {
-  auto body = IO::load_obj("C:\\Users\\USER\\source\\repos\\ml_4_mesh\\src\\Test\\Data\\aaa1.obj");
+  auto body = IO::load_obj("C:/Users/USER/source/repos/ml_4_mesh/src/Test/Data/aaa1.obj");
   flatten(body);
-  IO::save_obj("C:\\Users\\USER\\source\\repos\\ml_4_mesh\\src\\Test\\Data\\bbb1.obj", body);
+  IO::save_obj("C:/Users/USER/source/repos/ml_4_mesh/src/Test/Data/bbb1.obj", body);
+}
+
+TEST_CASE("flat_01", "[Flattening]")
+{
+  auto body = IO::load_obj("C:/Users/USER/source/repos/ml_4_mesh/src/Test/Data/aaa2.obj");
+  flatten(body);
+  IO::save_obj("C:/Users/USER/source/repos/ml_4_mesh/src/Test/Data/bbb2.obj", body);
 }
