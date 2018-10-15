d4c927d23f37ef547b449fb7ca1a777df145feec
galiautdinova gyuzel <guzel.gala@outlook.com>
2018-10-16 00:42:17 +0100

    It is slow, but it works

--- src/Test/lscm.cxx
+++ src/Test/lscm.cxx
@@ -13,6 +13,7 @@
 #include <Eigen/Sparse>
 #include <Eigen/SparseQR>
 
+#include "xxxxx.hxx"
 
 static auto move_to_local_coord(const Geo::VectorD3 _tri[3], Geo::VectorD2 _loc_tri[2])
 {
@@ -27,15 +28,36 @@ static auto move_to_local_coord(const Geo::VectorD3 _tri[3], Geo::VectorD2 _loc_
 }
 
 using VertexIndMpap = std::map<Topo::Wrap<Topo::Type::VERTEX>, size_t>;
-using Functor = Eigen::DenseFunctor<double>;
+using Functor = Eigen::SparseFunctor<double, int>;
 
 struct EnergyFunction : public Functor
 {
+  struct DataOfFace
+  {
+    Geo::VectorD2 loc_tri_[2];
+    size_t idx_[3];
+    double area_time_2_;
+  };
+
   EnergyFunction(const VertexIndMpap& _vrt_inds, 
     Topo::Iterator<Topo::Type::BODY, Topo::Type::FACE>& _bf, int _m, int _n) : 
-    Functor(_n, _m),
-    vrt_inds_(_vrt_inds), bf_(_bf)
-  {}
+    Functor(_n, _m), vrt_inds_(_vrt_inds)
+  {
+    for (auto face : _bf)
+    {
+      auto& fd = data_of_faces_.emplace_back();
+      // 3 equations per face.
+      Topo::Iterator<Topo::Type::FACE, Topo::Type::VERTEX> fv(face);
+      Geo::VectorD3 pts[3];
+      size_t i = 0;
+      for (auto v : fv)
+      {
+        v->geom(pts[i]);
+        fd.idx_[i++] = 2 * vrt_inds_.find(v)->second;
+      }
+      fd.area_time_2_ = move_to_local_coord(pts, fd.loc_tri_);
+    }
+  }
 
   int operator()(const Functor::InputType &_x, Functor::ValueType& _fvec) const
   {
@@ -47,42 +69,37 @@ struct EnergyFunction : public Functor
     return compute(_x, nullptr, &_fjac);
   }
 
-private:
+protected:
   const VertexIndMpap& vrt_inds_;
-  Topo::Iterator<Topo::Type::BODY, Topo::Type::FACE>& bf_;
+  std::vector<DataOfFace> data_of_faces_;
 
   int compute(const Functor::InputType& _x,
     Functor::ValueType* _fvec, Functor::JacobianType* _fjac) const
   {
-    if (_fjac != nullptr)
-      _fjac->setZero();
-    const size_t var_nmbr = vrt_inds_.size() * 2;
-    auto INDEX = [var_nmbr](size_t _i, size_t _j) { return _i * var_nmbr + _j; };
+    //if (_fjac != nullptr) _fjac->setZero();
+
+    auto X = [this, &_x](int _i)
+    {
+      if (_i < inputs())
+        return _x(_i);
+      else
+        return 0.;
+    };
+
     size_t i_eq_loop = 0;
-    for (auto face : bf_)
+    for (auto& fd : data_of_faces_)
     {
       // 3 equations per face.
       size_t i_eq = i_eq_loop;
       i_eq_loop += 3;
-      Topo::Iterator<Topo::Type::FACE, Topo::Type::VERTEX> fv(face);
-      size_t idx[3];
-      Geo::VectorD3 pts[3];
-      size_t i = 0;
-      for (auto v : fv)
-      {
-        v->geom(pts[i]);
-        idx[i++] = 2 * vrt_inds_.find(v)->second;
-      }
-      Geo::VectorD2 loc_tri[2];
-      auto area_time_2 = move_to_local_coord(pts, loc_tri);
-      auto u10 = _x(idx[1])     - _x(idx[0]);
-      auto u20 = _x(idx[2])     - _x(idx[0]);
-      auto v10 = _x(idx[1] + 1) - _x(idx[0] + 1);
-      auto v20 = _x(idx[2] + 1) - _x(idx[0] + 1);
-      double a = u10 / loc_tri[0][0];
-      double b = (u20 - loc_tri[1][0] * u10 / loc_tri[0][0]) / loc_tri[1][1];
-      double c = v10 / loc_tri[0][0];
-      double d = (v20 - loc_tri[1][0] * v10 / loc_tri[0][0]) / loc_tri[1][1];
+      auto u10 = X(fd.idx_[1])     - X(fd.idx_[0]);
+      auto u20 = X(fd.idx_[2])     - X(fd.idx_[0]);
+      auto v10 = X(fd.idx_[1] + 1) - X(fd.idx_[0] + 1);
+      auto v20 = X(fd.idx_[2] + 1) - X(fd.idx_[0] + 1);
+      double a = u10 / fd.loc_tri_[0][0];
+      double b = (u20 - fd.loc_tri_[1][0] * u10 / fd.loc_tri_[0][0]) / fd.loc_tri_[1][1];
+      double c = v10 / fd.loc_tri_[0][0];
+      double d = (v20 - fd.loc_tri_[1][0] * v10 / fd.loc_tri_[0][0]) / fd.loc_tri_[1][1];
       auto det = a * d - b * c;
       if (_fvec != nullptr)
       {
@@ -98,9 +115,9 @@ private:
              d / det, -c / det, -b / det, a / det;
 
       Eigen::Matrix<double, 4, 6> da_uv;
-      auto c0 = 1. / loc_tri[0][0]; // 1 / x_1
-      auto c1 = 1. / loc_tri[1][1]; // 1 / y_2
-      auto c2 = loc_tri[1][0] / (loc_tri[0][0] * loc_tri[1][1]); // x_2 / (x_1 * y_2)
+      auto c0 = 1. / fd.loc_tri_[0][0]; // 1 / x_1
+      auto c1 = 1. / fd.loc_tri_[1][1]; // 1 / y_2
+      auto c2 = fd.loc_tri_[1][0] / (fd.loc_tri_[0][0] * fd.loc_tri_[1][1]); // x_2 / (x_1 * y_2)
       da_uv <<
         -c0, 0, c0, 0, 0, 0,
         c2 - c1, 0, -c2, 0, c1, 0,
@@ -114,24 +131,58 @@ private:
           auto val = df_uv(i, j);
           if (val != 0)
           {
-            auto r = i_eq + i;
-            auto c = idx[j / 2] + j % 2;
-            _fjac->coeffRef(r, c) = val;
+            auto c = fd.idx_[j / 2] + j % 2;
+            if (c < inputs())
+              _fjac->coeffRef(i_eq + i, c) = val;
           }
         }
       }
     }
+    if (_fjac != nullptr) _fjac->makeCompressed();
+    if (_fvec != nullptr)
+    {
+      double sum = 0;
+      for (int i = 0; i < _fvec->rows(); ++i)
+        sum += (*_fvec)(i) * (*_fvec)(i);
+      std::cout << "F: " << sum << "\n\n";
+    }
+#ifdef PRINT
     if (_fvec != nullptr)
       std::cout << "F: " << *_fvec << "\n\n";
     if (_fjac != nullptr)
-    {
-      //_fjac->makeCompressed();
       std::cout << "df: " << *_fjac << "\n\n";
-    }
+#endif
     return 0;
   }
 };
 
+
+struct EnergySquare : public  IFunctionXXX, EnergyFunction
+{
+  using EnergyFunction::EnergyFunction;
+
+  EnergySquare(const VertexIndMpap& _vrt_inds,
+    Topo::Iterator<Topo::Type::BODY, Topo::Type::FACE>& _bf, int _m, int _n) :
+    EnergyFunction(_vrt_inds, _bf, _m, _n)
+  {
+    f_val_.resize(values(), 1);
+    jac_.resize(values(), inputs());
+  }
+
+  bool valuate(const Eigen::VectorXd& _x, double* _f, Eigen::VectorXd* _df) const
+  {
+    compute(_x, &f_val_, &jac_);
+    if (_f != nullptr)
+      *_f = (f_val_.transpose() * f_val_)(0, 0);
+    if (_df != nullptr)
+      *_df = 2 * (f_val_.transpose() * jac_);
+    return true;
+  }
+
+  mutable Eigen::VectorXd f_val_;
+  mutable Eigen::SparseMatrix<double> jac_;
+
+};
 
 struct OptimizeNonLinear : public IFunction
 {
@@ -171,7 +222,7 @@ struct OptimizeNonLinear : public IFunction
         idx[i++] = 2 * vrt_inds_.find(v)->second;
       }
       Geo::VectorD2 loc_tri[2];
-      auto area_time_2 = move_to_local_coord(pts, loc_tri);
+      auto area2 = move_to_local_coord(pts, loc_tri);
       auto u10 = _x[idx[1]] - _x[idx[0]];
       auto u20 = _x[idx[2]] - _x[idx[0]];
       auto v10 = _x[idx[1] + 1] - _x[idx[0] + 1];
@@ -301,9 +352,20 @@ void flatten(Topo::Wrap<Topo::Type::BODY> _body)
   static bool conformal = false;
   if (!conformal)
   {
-    EnergyFunction ef(vrt_inds, bf, 3 * bf.size(), vrt_inds.size() * 2);
-    Eigen::LevenbergMarquardt lm(ef);
+    int unknowns = cols - 3;
+    X.conservativeResize(unknowns);
+
+#if 1
+    EnergySquare es(vrt_inds, bf, 3 * bf.size(), unknowns);
+    minimize(X, es);
+#else
+    EnergyFunction ef(vrt_inds, bf, 3 * bf.size(), unknowns);
+    Eigen::LevenbergMarquardt_xxx lm(ef);
     auto info = lm.lmder1(X);
+#endif
+
+    X.conservativeResize(cols);
+    X(cols - 1) = X(cols - 2) = X(cols - 3) = 0;
 
 #if 0
     OptimizeNonLinear onl(vrt_inds, _body);
@@ -334,4 +396,11 @@ TEST_CASE("flat_02", "[Flattening]")
   auto body = IO::load_obj("C:/Users/USER/source/repos/ml_4_mesh/src/Test/Data/aaa2.obj");
   flatten(body);
   IO::save_obj("C:/Users/USER/source/repos/ml_4_mesh/src/Test/Data/bbb2.obj", body);
+}
+
+TEST_CASE("flat_03", "[Flattening]")
+{
+  auto body = IO::load_obj("C:/Users/USER/source/repos/ml_4_mesh/src/Test/Data/aaa3.obj");
+  flatten(body);
+  IO::save_obj("C:/Users/USER/source/repos/ml_4_mesh/src/Test/Data/bbb3.obj", body);
 }
